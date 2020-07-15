219230e9437dc55fe63dfaebc2b7cbb2c6a5d436
Kostyantyn Khomko <kostyanius@gmail.com>
2020-07-15 05:08:13 +0300

    Update vault.sh

--- vault.sh
+++ vault.sh
@@ -29,19 +29,22 @@ sleep 5
 
 /bin/vault status
 
-
 #aws_access_key_id=$(egrep -i "aws_access_key_id" ~/.aws/credentials | awk -F ' = ' '{print $2}')
 #aws_secret_access_key=$(egrep -i "aws_secret_access_key" ~/.aws/credentials | awk -F ' = ' '{print $2}')
 
 echo -e " \n"
 
+# Read AWS credentials
+
 read -p 'Enter aws_access_key_id: ' aws_access_key_id
 read -p 'Enter aws_secret_access_key: ' aws_secret_access_key
+
+# Passing AWS credentials to Vault
 
 vault kv put secret/id aws_access_key_id=$aws_access_key_id
 vault kv put secret/key aws_secret_access_key=$aws_secret_access_key
 
+# Getting AWS credentials from Vault
+
 vault kv get secret/id
 vault kv get secret/key
-
-
